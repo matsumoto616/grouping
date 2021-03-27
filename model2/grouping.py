13e16bbb26ed267abb6f09fc93455acaa17ea515
neiNEI1995 <00mtakuya00@gmail.com>
2021-03-27 16:45:09 +0900

    平準化を図るため目的関数の重みづけを作成

--- model2/grouping.py
+++ model2/grouping.py
@@ -4,21 +4,21 @@ import itertools
 
 # メンバー
 members = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-#members = ['A','B','C','D','E','F','G','H','I','J','K']
+members = members[:20]
 number_of_people = len(members)
 people_indices = list(range(number_of_people))
-combi_indices = list(itertools.combinations(people_indices,2)) # メンバーの組合せ
+combi = list(itertools.combinations(people_indices,2)) # メンバーの組合せ
 
 # グループ
-groups = ['G1','G2','G3']
+groups = ['G1','G2','G3','G4']
 number_of_group = len(groups)
 group_indices = list(range(number_of_group))
 
 # グループ分け回数
-number_of_times = 3
+number_of_times = 4
 time_indices = list(range(number_of_times))
 
-# グループ毎の人数を決定（できるだけ平均化する，もっと上手く書けない？）
+# グループ毎の人数を決定（できるだけ平均化する，ライブラリとか組込関数とかでもっと綺麗にかけそう...）
 group_capacity = np.zeros(number_of_group)
 q, mod = divmod(number_of_people,number_of_group)
 for j in group_indices:
@@ -30,17 +30,20 @@ for j in group_indices:
 def main():
     # z_i：コンビiが一度でも同じグループになったことがあるとき1,そうでないとき0 (これを決定変数としたものがmodel1)
     # 0で初期化
-    z0 = np.zeros(len(combi_indices))
+    z0 = np.zeros(len(combi))
     
     # 各班分けごとに最適化し，zを更新していく
     for step in range(number_of_times):
         z = solve(z0,step)
         z0 = z 
 
+# 心臓部
 # 前回までの班分けによって誰と一緒になったかの情報(z)を既知とし，次回の班分けを決定する
 def solve(z,step):
 
-    # 問題インスタンス生成
+    #####################################################################################
+    ###  線形計画問題インスタンス生成（本問題は0-1整数計画になる）
+    #####################################################################################
     prob = pulp.LpProblem(sense=pulp.LpMaximize)
 
     #####################################################################################
@@ -58,7 +61,7 @@ def solve(z,step):
     # 決定変数y_ij：コンビi(メンバーの組合せ)がグループjに入るとき1,それ以外のとき0
     y = pulp.LpVariable.dicts(
         'y',
-        (combi_indices,group_indices),
+        (combi,group_indices),
         0,
         1,
         pulp.LpBinary
@@ -79,16 +82,25 @@ def solve(z,step):
 
     # yはi1とi2がどちらもグループjなら1，そうでないとき0
     for j in group_indices:
-        for (i1,i2) in combi_indices:
+        for (i1,i2) in combi:
             prob += 2*y[(i1,i2)][j] <= x[i1][j] + x[i2][j]
             prob += x[i1][j] + x[i2][j] <= y[(i1,i2)][j] + 1
                     
     #####################################################################################
     ###  目的関数
     #####################################################################################
-    # 一回も同じグループになったことのないコンビ(z=0)ができるだけ同じグループ(y=1)になるように目的関数を設定（これを最大化する）
-    prob += pulp.lpSum([(1-z[c])*y[(i1,i2)][j] for c, (i1,i2) in enumerate(combi_indices) for j in group_indices])
+    # 重みの計算（平準化を狙う）
+    weight = calc_weight(z)
 
+    # 一回も同じグループになったことのないコンビ(z=0)ができるだけ同じグループ(y=1)になるように目的関数を設定（これを最大化する）
+    # 特に不遇な人（同じ人とばかりグループになっている人）が優先されるように重みづける(weight[i1]+weight[i2]+1)
+    # ＋1しているのは重みの最小値が0にならないようにするため
+    prob += pulp.lpSum([(weight[i1]+weight[i2]+1)*(1-z[c])*y[(i1,i2)][j] for c, (i1,i2) in enumerate(combi) for j in group_indices])
+
+
+    #####################################################################################
+    ###  最適化実行
+    #####################################################################################
     # 一回目は辞書的に決定
     if step == 0:
         fix_initial_group(x,group_capacity)
@@ -106,17 +118,40 @@ def solve(z,step):
                                 maxSeconds=100,
                                 threads=None))
 
+    #####################################################################################
+    ###  後処理
+    #####################################################################################
     # zを更新
-    for c, (i1,i2) in enumerate(combi_indices):
+    for c, (i1,i2) in enumerate(combi):
         for j in group_indices:
             z[c] = max(z[c],y[(i1,i2)][j].value())
 
     # 結果出力
-    output_group(x,y,z)
+    gen_output(x,y,z,step)
 
     return z
 
-# 1回目は辞書的にグループを決定
+# できるだけ色々な人とグループになれるような，目的関数の重みを計算する
+# （主役がいる場合などは，それを最優先に考慮する）←未実装
+def calc_weight(z):
+    # 各メンバーに対して，一緒のグループになったことがある人数を計算
+    #（これが平準化されるように重みを決定する）
+    num_met = np.zeros(number_of_people)
+    for c, (i1,i2) in enumerate(combi):
+        if z[c] == 1:
+            num_met[i1] += 1
+            num_met[i2] += 1
+
+    # 最大値との差を重みとする（不遇な人ほど値が大きくなる）
+    weight = num_met.max() - num_met
+
+    # 重みを極端にすると，不遇な人を優先しすぎる恐れがあるので，気持ち小さく
+    weight *= 0.01
+
+    return weight
+
+
+# 1回目は辞書的にグループを決定（必要に応じてmembersリストをランダムシャッフルしてもよい）
 def fix_initial_group(x,group_capacity):
     count_global = 0   
     for j in group_indices:
@@ -132,15 +167,25 @@ def fix_initial_group(x,group_capacity):
         count_global += count
 
 # 結果の出力
-def output_group(x,y,z):
-    # zの総和出力（大きいほど1回も一緒にならなかった組合せが少ない）
-    print(f'z_sum = {z.sum()} / {len(combi_indices)} (max)')
+def gen_output(x,y,z,step):
+    
+    print(f'\n##### grouping {step} #####')
 
-    # グループの出力
+    # 得られたグループ分けの出力
     for j in group_indices:
         print(f'Group {groups[j]}: {[members[i] for i in people_indices if x[i][j].value() == 1]}')
-    # for c, (i1,i2) in enumerate(combi_indices):
 
+    # 各メンバーに対して，一緒のグループになったことがある人数を出力
+    #（数値が大きい程よい，出来れば平準化されているほうがよい）
+    num_met = np.zeros(number_of_people)
+    for c, (i1,i2) in enumerate(combi):
+        if z[c] == 1:
+            num_met[i1] += 1
+            num_met[i2] += 1
+    print(num_met)
+
+    # zの総和出力（大きいほど1回も一緒にならなかった組合せが少ない）
+    print(f'z_sum = {z.sum()} / {len(combi)} (max)')
 
 if __name__ == '__main__':
     main()
